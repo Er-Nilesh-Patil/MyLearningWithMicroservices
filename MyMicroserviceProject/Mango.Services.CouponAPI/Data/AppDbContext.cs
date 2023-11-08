fef93ed03c95fb94803abd94c6bf225eba9e365b
Nilesh Patil <ernileshpatil2016@gmail.com>
2023-11-08 17:51:47 +0530

    added migration to the mango-coupons table and all other details

---
+++ MyMicroserviceProject/Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,74 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Coupon> Coupones { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon()
+            {
+                CouponId = 1,
+                CouponCode = "1000A",
+                DiscountAmount = 100,
+                MinAmount = 500
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon()
+            {
+                CouponId = 2,
+                CouponCode = "1000B",
+                DiscountAmount = 10,
+                MinAmount = 50
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon()
+            {
+                CouponId = 3,
+                CouponCode = "1000C",
+                DiscountAmount = 150,
+                MinAmount = 500
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon()
+            {
+                CouponId = 4,
+                CouponCode = "1000D",
+                DiscountAmount = 140,
+                MinAmount = 200
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon()
+            {
+                CouponId = 5,
+                CouponCode = "1000E",
+                DiscountAmount = 10,
+                MinAmount = 50
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon()
+            {
+                CouponId = 6,
+                CouponCode = "1000F",
+                DiscountAmount = 180,
+                MinAmount = 350
+            });
+
+
+        }
+
+
+    }
+}
---
+++ MyMicroserviceProject/Mango.Services.CouponAPI/Migrations/20231108121508_seedCoupontable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Mango.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedCoupontable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Coupones",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount", "MinAmount" },
+                values: new object[,]
+                {
+                    { 1, "1000A", 100.0, 500 },
+                    { 2, "1000B", 10.0, 50 },
+                    { 3, "1000C", 150.0, 500 },
+                    { 4, "1000D", 140.0, 200 },
+                    { 5, "1000E", 10.0, 50 },
+                    { 6, "1000F", 180.0, 350 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupones",
+                keyColumn: "CouponId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Coupones",
+                keyColumn: "CouponId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Coupones",
+                keyColumn: "CouponId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Coupones",
+                keyColumn: "CouponId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Coupones",
+                keyColumn: "CouponId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Coupones",
+                keyColumn: "CouponId",
+                keyValue: 6);
+        }
+    }
+}
